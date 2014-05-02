74149c42cac856a6d4037b8e1d92ae397b8b9557
Ronny Trommer <ronny@opennms.org>
2014-05-03 01:05:12 +0200

    Fix node attribute for java heap in opennms-remote-poller init script

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ronny@opennms.org'
 license          'Apache 2.0'
 description      'Installs/Configures opennms'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 recipe 'opennms', 'Installs open source enterprise network management platform OpenNMS'
 
