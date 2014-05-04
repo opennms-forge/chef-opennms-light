09ec90480a90ca74894be047e7354ed106b9e8c9
Ronny Trommer <ronny@opennms.org>
2014-05-04 19:56:15 +0200

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ronny@opennms.org'
 license          'GPLv3+'
 description      'Installs/Configures opennms'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.9'
 
 recipe 'opennms', 'Installs open source enterprise network management platform OpenNMS'
 
