e2d3646c4f1a39e354f9368bccee781f7f550e20
Ronny Trommer <ronny@opennms.org>
2014-10-31 11:41:43 +0100

    Install Java and Postgres after repository setup to avoid install with outdated repository.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,6 @@
 # License GPLv3
 #
 
-include_recipe "java"
-include_recipe "postgresql::server"
-
 case node['platform_family']
 # Install yum repository on Red Hat family linux
 when "rhel"
@@ -53,7 +50,11 @@ when "debian"
   end
 end
 
-# Install OpenNMS and Java RRDtool library
+# Install basic dependencies
+include_recipe "java"
+include_recipe "postgresql::server"
+
+# Install OpenNMS and Java-RRDtool library
 ["opennms",
  "jrrd",
  "rrdtool"].each do |package_name|
